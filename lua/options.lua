c1ba7faf53947cbde2aeaf5d66df22e61c969060
Klishin Maksim <klishinmaksim@internet.ru>
2024-09-22 12:56:39 +0300

    init project

---
+++ lua/options.lua
@@ -0,0 +1,21 @@
+local o = vim.opt
+local g = vim.g
+
+g.mapleader = ' '
+g.maplocalleader = '\\'
+
+o.number = true
+o.relativenumber = true
+o.cursorline = true
+o.scrolloff = 5
+
+o.splitbelow = true
+o.splitright = true
+
+o.ignorecase = true
+o.smartcase = true
+
+o.hidden = true
+o.undofile = true
+
+o.clipboard = 'unnamedplus'
